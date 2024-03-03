8a26b944ae49bc191a8b40efa0873b6d2886f396
DANG MINH <minhdangngoc304@gmail.com>
2024-03-03 15:51:41 +0700

    create layout main_activity

---
+++ app/src/main/java/com/movieapi/movie/MainActivity.java
@@ -0,0 +1,65 @@
+package com.movieapi.movie;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
+import com.movieapi.movie.Fragment.FavouritesFragment;
+import com.movieapi.movie.Fragment.MovieFragment;
+import com.movieapi.movie.Fragment.SearchFragment;
+import com.movieapi.movie.Fragment.SeriesFragment;
+import com.movieapi.movie.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MovieFragment()).commit();
+        binding.toolbarMain.setTitle(getString(R.string.movies_nav));
+
+        binding.bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_movie:
+                        if(!getSupportFragmentManager().findFragmentById(R.id.fragment_container).getClass().getSimpleName().equals("MovieFragment")){
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MovieFragment()).commit();
+                            binding.toolbarMain.setTitle(getString(R.string.movies_nav));
+                        }
+                        break;
+
+                    case R.id.nav_series:
+                        if(!getSupportFragmentManager().findFragmentById(R.id.fragment_container).getClass().getSimpleName().equals("SeriesFragment")){
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SeriesFragment()).commit();
+                            binding.toolbarMain.setTitle(getString(R.string.series_nav));
+                        }
+                        break;
+
+                    case R.id.nav_search:
+                        if(!getSupportFragmentManager().findFragmentById(R.id.fragment_container).getClass().getSimpleName().equals("SearchFragment")){
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SearchFragment()).commit();
+                            binding.toolbarMain.setTitle(getString(R.string.search_nav));
+                        }
+                        break;
+
+                    case R.id.nav_favourites:
+                        if(!getSupportFragmentManager().findFragmentById(R.id.fragment_container).getClass().getSimpleName().equals("FavouritesFragment")){
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FavouritesFragment()).commit();
+                            binding.toolbarMain.setTitle(getString(R.string.favourites_nav));
+                        }
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+}
